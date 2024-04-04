ea0c3d7d49f5911a62707f6fa33c6637b5173da3
ProbirKumare <probirsharma655@gmail.comm>
2024-04-04 16:50:39 +0600

    btn add shortcut

--- src/Components/Cosmetic/Cosmetic.js
+++ src/Components/Cosmetic/Cosmetic.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import './Cosmetic.css'
 
 const Cosmetic = (props) => {
     // console.log(props);
     const { id, name, price } = props.cosmetic;
+
+    const addToCart = (id) =>{
+        console.log('clicked item', id)
+    }
+
+    // const addToCartWithParam = () => addToCart(id);
+
     return (
-        <div className=''>
+        <div className='projuct'>
             <h1>Name: {name}</h1>
             <p>Id: {id}</p>
             <p>Price: ${price}</p>
+            {/* <button onClick={addToCartWithParam}>Add To Cart</button> */}
+            <button onClick={() => addToCart(id)}>Add To Cart</button>
         </div>
     );
 };
